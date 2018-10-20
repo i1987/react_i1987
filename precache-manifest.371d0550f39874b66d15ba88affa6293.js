3a6e94c88ac7f251407b309a8e1af2097bb49e46
lianghuiyuan <creasy@molmc.com>
2018-10-20 11:35:12 +0800

    Updates

---
+++ precache-manifest.371d0550f39874b66d15ba88affa6293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lianghuiyuan/i1987.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee8c0b5f4cb506455a7a",
+    "url": "/lianghuiyuan/i1987.git/static/js/runtime~main.ee8c0b5f.js"
+  },
+  {
+    "revision": "2663199ebcef6fd26896",
+    "url": "/lianghuiyuan/i1987.git/static/js/main.2663199e.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/lianghuiyuan/i1987.git/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "2663199ebcef6fd26896",
+    "url": "/lianghuiyuan/i1987.git/static/css/main.d87f2606.chunk.css"
+  },
+  {
+    "revision": "2b1b892b7c1f7721312010903472fbc7",
+    "url": "/lianghuiyuan/i1987.git/index.html"
+  }
+];
